da122beebfc74164e043d10d97d75a882e41f844
Franck Blettner <franck.blettner@nomana-it.fr>
2025-03-07 16:48:28 +0100

    Rebuild to use liberty framework package instead of cloning

--- backend/app/dags/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-# Get the base directory of the package
-BASE_DIR = Path(__file__).resolve().parent
-
-def get_dags_path():
-    """Return the absolute path to logs-frontend-text.log"""
-    return str(BASE_DIR)
